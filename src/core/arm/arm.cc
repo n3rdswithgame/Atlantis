becda8d673a0f3661d7e893b97abf3d7314edcd6
N3rdsWithGame <n3rdswithgame@gmail.com>
2019-01-31 17:32:12 -0800

    updated lifters
    rewrote the lifters to take advantage of the templated mmu

--- src/core/arm/arm.cc
+++ src/core/arm/arm.cc
@@ -5,96 +5,13 @@
 
 #include "common/logger.h"
 
+#include "../mem.h"
+
 //TODO: write unit tests
 
 namespace arm {
 
-	Lifter::Lifter(mmu::mmu & m) {
-		mmu = &m;
-		if(cs_err ret = cs_open(CS_ARCH_ARM, CS_MODE_ARM, &cap_arm);
-			ret != CS_ERR_OK) {
-			FATAL("Error creating ARM capstone engine with error code: {}", ret);
-		}
-
-		if(cs_err ret = cs_open(CS_ARCH_ARM, CS_MODE_THUMB, &cap_thumb);
-			ret != CS_ERR_OK) {
-			FATAL("Error creating THUMB capstone engine with error code: {}", ret);
-		}
-	}
-	Lifter::Lifter(Lifter&& l) {
-		mmu = std::exchange(l.mmu, nullptr);
-		cap_arm = l.cap_arm;
-		cap_thumb = l.cap_thumb;
-	}
-	Lifter::~Lifter() {
-		if(mmu != nullptr) {
-			cs_close(&cap_arm);
-			cs_close(&cap_thumb);
-		}
-	}
-
-	arm_ins_t Lifter::fetch_impl(addr_t pc) {
-		if((pc & 1) == 0)
-			return fetchArm_impl(pc);
-		else
-			return fetchThumb_impl(pc & (~1U)); // translate pc into address
-	}
-
-	arm_ins_t Lifter::fetchArm_impl(addr_t addr) {
-		return fetchArch_impl<isa::arm>(addr);
-	}
-
-	arm_ins_t Lifter::fetchThumb_impl(addr_t addr) {
-		return fetchArch_impl<isa::thumb>(addr); //pc is already translated into an address in fetch_impl
-	}
-
-
-	template<isa i>
-	arm_ins_t Lifter::fetchArch_impl(addr_t addr) {
-		constexpr size_t num_inst = 1;
-		constexpr bool is_arm = (i == isa::arm);
-
-		cs_insn *insn;
-		size_t count;
-
-		u32 raw_machine_code; //for the log message
-		if constexpr(is_arm) {
-			u32 machine_code = mmu->read<u32>(addr).read_val;//no static_cast as read_val is already a reg_t aka u32
-			raw_machine_code = machine_code;
-			count = cs_disasm(cap_arm, reinterpret_cast<u8*>(&machine_code),
-				sizeof(machine_code), addr, num_inst, &insn);
-		} else {
-			u16 machine_code = static_cast<u16>(mmu->read<u16>(addr).read_val);
-			raw_machine_code = machine_code;
-			count = cs_disasm(cap_thumb, reinterpret_cast<u8*>(&machine_code),
-				sizeof(machine_code), addr, num_inst, &insn);
-		}
-
-		if (count == 0) {
-			FATAL("Failure to disassemble {0} machine code into instructions\n"
-				  "\taddr: 0x{1:08x}\n"
-				  "\t raw: 0x{2:08x}",
-				  i,
-				  addr,
-				  raw_machine_code);
-			return {};
-		}
-
-		arm_ins_t ins{};
-		ins.addr = addr;
-
-		if constexpr(is_arm) {
-			decodeArm(ins, insn[0].mnemonic, insn[0].op_str);
-		} else {
-			decodeThumb(ins, insn[0].mnemonic, insn[0].op_str);
-		}
-
-		cs_free(insn, num_inst);
-
-		return ins;
-	}
-
-	void Lifter::decodeArm(arm_ins_t& ins, std::string_view mnemonic, std::string_view op_str) {
+	void decodeArm(arm_ins_t& ins, std::string_view mnemonic, std::string_view op_str) {
 		//TODO: remove reference captures once fully implemented
 		//this 1st parser would be alot easier if std::s_v::ends_with existed yet	
 		auto conditionalStripper =
@@ -156,7 +73,9 @@ namespace arm {
 
 		auto operandParser = [&](std::string_view opstr) -> std::vector<operand_t> {
 			//TODO: implement
-			FATAL("operandParser not implemented yet");
+			FATAL("operandParser not implemented yet so cannot parse \n"
+				"\t{}",
+				opstr);
 			std::exit(-1);
 			return {};
 		};
@@ -166,8 +85,12 @@ namespace arm {
 		ins.op = mnemonicParser(mne);
 		ins.operands = operandParser(op_str);
 	}
-	void Lifter::decodeThumb(arm_ins_t&, std::string_view mnemonic, std::string_view op) {
-
+	void decodeThumb(arm_ins_t& ins, std::string_view mnemonic, std::string_view op_str) {
+		FATAL("Lifter::decodeThumb is unimplemented so cannout parse \n"
+			"\t{}\t{}",
+			mnemonic, op_str);
+		std::exit(-1);
+		ins.cond = arm::cond::AL; //Not until ARMv6 thumb got the IT instruction (and therefore got conditional inst)
 	}
 
 } //namespace arm
--- src/core/ast/lifter.h
+++ src/core/ast/lifter.h
@@ -1,11 +1,37 @@
 #ifndef LIFTER_H
 #define LIFTER_H
 
+#include <algorithm>
+
+#include "common/types.h"
+#include "../mem.h"
+
 namespace ast {
-	template<class T, typename isa_t, class CRTP>
+	template<class ins_t, typename isa_t, class CRTP, class mmu_t>
 	class Lifter {
-		#define crpt (static_cast<CRTP*>(this))
-		
+	#define crtp (static_cast<CRTP*>(this))
+		mmu_t* mmu;
+
+	public:
+		Lifter(mmu_t* m) : mmu(m) {}
+		Lifter(Lifter&& l) {
+			mmu = std::exchange(l.mmu, nullptr);
+		}
+
+		bool isValid() {
+			return (mmu != nullptr);
+		}
+
+		ins_t fetch(addr_t addr) {
+			return crtp->fetch_impl(addr);
+		}
+	protected:
+		template<typename T>
+		auto mmuFetch(addr_t addr) -> mem::read_ret<T>{
+			return mmu->read(addr);
+		}
+
+	#undef crtp
 	};
 } //namespace ast
 
