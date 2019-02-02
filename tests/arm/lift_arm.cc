df4f106ed5668c6ea884d9ea7b10e56f7640a251
N3rdsWithGame <n3rdswithgame@gmail.com>
2019-02-01 18:21:16 -0800

    Update header paths
    Updated header paths to try and remove "../" in local headers

--- tests/arm/lift_arm.cc
+++ tests/arm/lift_arm.cc
@@ -1,8 +1,8 @@
 #include <catch2/catch.hpp>
 
-#include "../../src/core/mmu.h"
+#include "core/mmu.h"
 
-#include "../../src/core/arm/arm.h"
+#include "core/arm/arm.h"
 
 TEST_CASE("Testing decodeing + lifting of arm inst", "[arm lifting]") {
 	REQUIRE(1 == 1);
