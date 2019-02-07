ef11b32f58f10a831a9e9b72871e648a5136514b
N3rdsWithGame <n3rdswithgame@gmail.com>
2019-02-06 17:32:31 -0800

    Added some instructions + cleaned up previous instruction
    Added branch + branch and link  instructions
    
    cleaned up the 16 data processing instructions to be a bit cleaner. Also moved conditional into its own <op>_cond symbol

--- src/atlantis/main.cc
+++ src/atlantis/main.cc
@@ -3,7 +3,7 @@
 #include <string>
 #include <type_traits>
 
-//#include <clara.hpp>
+#include <clara.hpp>
 
 #include "cli.h"
 #include "ver.h"
@@ -16,79 +16,54 @@
 #include "core/arm/ins/ins.h"
 
 
-//std::optional<atlantis::cli_args> parseArgs(int argc, char** argv) {
-//	using namespace clara;
-//	
-//	bool show_help = false;
-//	bool show_ver = false;
-//
-//	atlantis::cli_args cli_args;
-//	auto cli = Opt(cli_args.rom_name, "rom")["-r"]["--rom-name"]("the rom to open")
-//		| Opt(show_ver)["-v"]["--ver"]["--version"]("show the version")
-//		| Help(show_help)
-//	;
-//
-//	auto result = cli.parse(Args(argc, argv));
-//
-//
-//	if(!result){
-//		DEBUG("failed to parse commandline args");
-//		return std::nullopt;
-//	}
-//
-//	if(show_help) {
-//		std::cout << cli << "\n";
-//		return std::nullopt;
-//	} else if(show_ver) {
-//		std::cout << "atlantis: version "<< VERSION <<"\n";
-//		return std::nullopt;
-//	}
-//
-//	return cli_args;
-//}
+std::optional<atlantis::cli_args> parseArgs(int argc, char** argv) {
+	using namespace clara;
+	
+	bool show_help = false;
+	bool show_ver = false;
 
-template<typename T>
-void test() {
-	T  i(arm::cpu::reg::r1, arm::cpu::reg::r0, arm::cpu::reg::r2);
-	DEBUG("{:08x}", static_cast<size_t>(i) );
+	atlantis::cli_args cli_args;
+	auto cli = Opt(cli_args.rom_name, "rom")["-r"]["--rom-name"]("the rom to open")
+		| Opt(show_ver)["-v"]["--ver"]["--version"]("show the version")
+		| Help(show_help)
+	;
+
+	auto result = cli.parse(Args(argc, argv));
+
+
+	if(!result){
+		FATAL("failed to parse commandline args");
+		return std::nullopt;
+	}
+
+	if(show_help) {
+		std::cout << cli << "\n";
+		return std::nullopt;
+	} else if(show_ver) {
+		std::cout << "atlantis: version "<< VERSION <<"\n";
+		return std::nullopt;
+	}
+
+	return cli_args;
 }
+
 
 int main(int argc, char** argv)
 {
-	(void)argc;
-	(void)argv;	
+	auto cli_args = parseArgs(argc, argv);
 
-	test<arm::ins::Add>();
-	test<arm::ins::AddS>();
-	
-	test<arm::ins::Eor>();
-	test<arm::ins::EorS>();
-	
-	test<arm::ins::Mov>();
-	test<arm::ins::MovS>();
-	
-	test<arm::ins::Rsb>();
-	test<arm::ins::RsbS>();
-	
-	test<arm::ins::Mvn>();
-	test<arm::ins::MvnS>();
+	if(!cli_args) {
+		return -1;
+	}
 
+	atlantis::cli_args& args = *cli_args;
 
+	if(args.rom_name == "") {
+		FATAL("rom name was not passed");
+		return -1;
+	}
 
-	//auto cli_args = parseArgs(argc, argv);
-
-	//if(!cli_args) {
-	//	return -1;
-	//}
-
-	//atlantis::cli_args& args = *cli_args;
-
-	//if(args.rom_name == "") {
-	//	FATAL("rom name was not passed");
-	//	return -1;
-	//}
-
-	//gba::gba gba(args.rom_name);
+	gba::gba gba(args.rom_name);
 
     return 0;
 }
--- src/core/arm/ins/ins.h
+++ src/core/arm/ins/ins.h
@@ -45,6 +45,10 @@ namespace arm::ins {
 										bit<1, 4>
 									>;
 
+		using BranchImm				= combine<
+										bit_range<0b101, 27, 25>
+									>;
+
 		using ImmtoStatusReg		= combine<
 										bit_range<0b00110, 27, 23>,
 										bit_range<0b10, 21, 20>
@@ -73,9 +77,6 @@ namespace arm::ins {
 										bit_range<0b100, 27, 25>
 									>;
 
-		using BranchImm				= combine<
-										bit_range<0b101, 27, 25>
-									>;
 		using Media					= combine<
 										bit_range<0b011, 27, 25>,
 										bit<1, 4>
@@ -189,252 +190,262 @@ namespace arm::ins {
 			asl = mask::bit_range<0b10, 6, 5>::m,
 			asr = mask::bit_range<0b01, 6, 5>::m,
 		};
-	}//namespace arm::ins::flags
 
-	template<u32 Armv, typename Mask>
-	struct ArmInst : Mask {
-		constexpr static u32 ver = Armv;
-		constexpr static u32 getVer() {
-			return ver;
-		}
+		enum class link {
+			link	= mask::bit<1, 24>::m,
+			ignore	= mask::bit<0, 24>::m,
+		};
+	}//namespace arm::ins::parts
 
-		bool used = false;
-		u32 value = 0;
+namespace types {
 
-		constexpr ArmInst() { 
-			//default used to false
-			used = false;
-		}
-		constexpr ArmInst(u32 v) {
-			used = true;
-			value = v;
-		}
-
-		constexpr u32 getVal() const {
-			return false;
-		}
-
-		constexpr operator u32() const {
-			return getVal();
-		}
-
-	protected:
-		constexpr u32 getConditionalVal() const {
-		//for the many struct below, only contribute to the encoded
-		//value if its used
-			if(used) {
-				return value;
-			} else {
-				return 0;
-			}
-		}
-
-		constexpr u32 getConditionalVer() const {
-		//for the many struct below, only contribute to the versioning
-		//if your encoding is used
-			if(used)
+		template<u32 Armv, typename Mask>
+		struct ArmInst : Mask {
+			constexpr static u32 ver = Armv;
+			constexpr static u32 getVer() {
 				return ver;
-			else
-				return 0;
-		}
-	};
-
-	template<typename Arminst, parts::cond cond=parts::cond::eq>
-	struct Conditional : Arminst{
-		using Arminst::Arminst;
-
-		using Arminst::getVer;
-
-		constexpr u32 getVal() const {
-			return static_cast<u32>(cond) | Arminst::operator u32();
-		}
-
-		constexpr operator u32() const {
-			return getVal();
-		}
-	};
-
-	// DO NOT USE. This is only left to show how to implement an instruction / 
-	// group of instrucitons that has multiple possible encodigns depending on 
-	// args
-	//
-	//template<typename... ArmInsts>
-	//struct Many : ArmInsts... {
-	//	//import all of the ctors for use in above
-	//	//ctor "spealiziation"
-	//	template<typename... Args>
-	//	constexpr Many(Args... args) : ArmInsts(args...) {}
-
-	//	constexpr operator u32() const {
-	//	//shadow the operator u32 in the super classes
-	//		return (ArmInsts::getConditionalVal() | ...);
-	//	}
-
-	//	constexpr u32 getVer() const {
-	//	//As mentioned above, this will or all of the conditional versions together,
-	//		return (ArmInsts::getConditionalVer() | ...);
-	//	}
-	//};
-
-	constexpr u32 RdRmEnc(cpu::reg rd, cpu::reg rn) {
-		return BIT_PLACE(rn, 19, 16) | BIT_PLACE(rd, 15, 12);
-	}
-
-	constexpr u32 ImmShiftEnc(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) {
-		return RdRmEnc(rd,rn) | BIT_PLACE(shift_imm, 11, 7) | C(sh) | C(shift_imm) | BIT_PLACE(rm, 3, 0);
-	}
-
-	template<u32 Armv, parts::dp op, parts::status s>
-	struct DPImmShift : ArmInst<Armv,mask::DPImmShift> {
-		constexpr DPImmShift() : ArmInst<Armv, mask::DPImmShift>()
-		{}
-
-		constexpr DPImmShift(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) :
-			ArmInst<Armv, mask::DPImmShift>(C(op) | C(s) | ImmShiftEnc(rd, rn, rm, sh, shift_imm))
-		{}
-	};
+			}
 	
-	constexpr u32 RegShiftEnc(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) {
-		return RdRmEnc(rd, rn) | BIT_PLACE(rs, 11, 8) | C(sh) | BIT_PLACE(rm, 3, 0);
-	}
-
-	template<u32 Armv, parts::dp op, parts::status s>
-	struct DPRegShift : ArmInst<Armv,mask::DPRegShift> {
-		constexpr DPRegShift() : ArmInst<Armv, mask::DPRegShift>()
-		{}
-
-		constexpr DPRegShift(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) : 
-			ArmInst<Armv, mask::DPRegShift>(C(op) | C(s) | RegShiftEnc(rd, rn, rm, sh, rs))
-		{}
-	};
-
-
-	constexpr u32 RotImmEnc(u8 rot, u8 imm) {
-		return BIT_PLACE(rot, 11, 8) | BIT_PLACE(imm, 7, 0);
-	}
-
-	template<u32 Armv, parts::dp op, parts::status s>
-	struct DPImm : ArmInst<Armv,mask::DPImm> {
-		constexpr DPImm() : ArmInst<Armv, mask::DPImm>()
-		{}
-
-		constexpr DPImm(cpu::reg rd, cpu::reg rn, u8 rot, u8 imm) : 
-			ArmInst<Armv, mask::DPImm>(C(op) | C(s) | RotImmEnc(rot, imm))
-		{}	
-	};
-
-	constexpr cpu::reg DPDefaultRN(parts::dp op, cpu::reg rd) {
-		if(op == parts::dp::Mov)
-			return cpu::reg::r0;
-		return rd;
-	}
-
-	template<u32 Armv, parts::dp op, parts::status s>
-	struct DataProcessing : DPImmShift<Armv, op, s>,
-							DPRegShift<Armv, op, s>,
-							DPImm<Armv, op, s>
-	{
-		using DPImmShift = arm::ins::DPImmShift<Armv, op, s>;
-		using DPRegShift = arm::ins::DPRegShift<Armv, op, s>;
-		using DPImm 	 = arm::ins::DPImm<Armv, op, s>;
+			bool used = false;
+			u32 value = 0;
+	
+			constexpr ArmInst() { 
+				//default used to false
+				used = false;
+			}
+			constexpr ArmInst(u32 v) {
+				used = true;
+				value = v;
+			}
+	
+			constexpr u32 getVal() const {
+				return false;
+			}
+	
+			constexpr operator u32() const {
+				return getVal();
+			}
+	
+		protected:
+			constexpr u32 getConditionalVal() const {
+			//for the many struct below, only contribute to the encoded
+			//value if its used
+				if(used) {
+					return value;
+				} else {
+					return 0;
+				}
+			}
+	
+			constexpr u32 getConditionalVer() const {
+			//for the many struct below, only contribute to the versioning
+			//if your encoding is used
+				if(used)
+					return ver;
+				else
+					return 0;
+			}
+		};
+	
+		template<typename Arminst, parts::cond cond>
+		struct Conditional : Arminst{
+			using Arminst::Arminst;
+	
+			using Arminst::getVer;
+	
+			constexpr u32 getVal() const {
+				return static_cast<u32>(cond) | Arminst::operator u32();
+			}
+	
+			constexpr operator u32() const {
+				return getVal();
+			}
+		};
+	
+		// TODO: figure out how to SFINAE between the two constructors in many_wrapper
+		// on whether the template class has a constructor that can accept that arguments
+		// This will clean up the DataProcessing class
 		
-		constexpr DataProcessing(cpu::reg rd,              cpu::reg rm) :
-			DataProcessing(rd, DPDefaultRN(op, rd), rm)
-		{}
+		//template<typename T>
+		//struct many_wrapper : T {
+		//	template<typename... Args>
+		//	many_wrapper(Args&&... args) : T(std::forward<Args>(args)...) {}
+	
+		//	template<typename... Args>
+		//	many_wrapper(Args&&... args) : T() {}
+		//}
+	
+		//template<typename... ArmInsts>
+		//struct Many : many_wrapper<ArmInsts>... {
+		//	//import all of the ctors for use in above
+		//	//ctor "spealiziation"
+		//	template<typename... Args>
+		//	constexpr Many(Args... args) : many_wrapper<ArmInsts>(args...) {}
+	
+		//	constexpr operator u32() const {
+		//	//shadow the operator u32 in the super classes
+		//		return (ArmInsts::getConditionalVal() | ...);
+		//	}
+	
+		//	constexpr u32 getVer() const {
+		//	//As mentioned above, this will or all of the conditional versions together,
+		//		return (ArmInsts::getConditionalVer() | ...);
+		//	}
+		//};
+	
+		constexpr u32 RdRmEnc(cpu::reg rd, cpu::reg rn) {
+			return BIT_PLACE(rn, 19, 16) | BIT_PLACE(rd, 15, 12);
+		}
+	
+		constexpr u32 ImmShiftEnc(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) {
+			return RdRmEnc(rd,rn) | BIT_PLACE(shift_imm, 11, 7) | C(sh) | C(shift_imm) | BIT_PLACE(rm, 3, 0);
+		}
+	
+		template<u32 Armv, parts::dp op, parts::status s>
+		struct DPImmShift : ArmInst<Armv,mask::DPImmShift> {
+			constexpr DPImmShift() : ArmInst<Armv, mask::DPImmShift>()
+			{}
+	
+			constexpr DPImmShift(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) :
+				ArmInst<Armv, mask::DPImmShift>(C(op) | C(s) | ImmShiftEnc(rd, rn, rm, sh, shift_imm))
+			{}
+		};
+		
+		constexpr u32 RegShiftEnc(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) {
+			return RdRmEnc(rd, rn) | BIT_PLACE(rs, 11, 8) | C(sh) | BIT_PLACE(rm, 3, 0);
+		}
+	
+		template<u32 Armv, parts::dp op, parts::status s>
+		struct DPRegShift : ArmInst<Armv,mask::DPRegShift> {
+			constexpr DPRegShift() : ArmInst<Armv, mask::DPRegShift>()
+			{}
+	
+			constexpr DPRegShift(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) : 
+				ArmInst<Armv, mask::DPRegShift>(C(op) | C(s) | RegShiftEnc(rd, rn, rm, sh, rs))
+			{}
+		};
+	
+	
+		constexpr u32 RotImmEnc(u8 rot, u8 imm) {
+			return BIT_PLACE(rot, 11, 8) | BIT_PLACE(imm, 7, 0);
+		}
+	
+		template<u32 Armv, parts::dp op, parts::status s>
+		struct DPImm : ArmInst<Armv,mask::DPImm> {
+			constexpr DPImm() : ArmInst<Armv, mask::DPImm>()
+			{}
+	
+			constexpr DPImm(cpu::reg rd, cpu::reg rn, u8 rot, u8 imm) : 
+				ArmInst<Armv, mask::DPImm>(C(op) | C(s) | RotImmEnc(rot, imm))
+			{}	
+		};
+	
+		constexpr cpu::reg DPDefaultRN(parts::dp op, cpu::reg rd) {
+			if(op == parts::dp::Mov)
+				return cpu::reg::r0;
+			return rd;
+		}
+	
+		template<u32 Armv, parts::dp op, parts::status s>
+		struct DataProcessing : DPImmShift<Armv, op, s>,
+								DPRegShift<Armv, op, s>,
+								DPImm<Armv, op, s>
+		{
+			using DPImmShift = arm::ins::types::DPImmShift<Armv, op, s>;
+			using DPRegShift = arm::ins::types::DPRegShift<Armv, op, s>;
+			using DPImm 	 = arm::ins::types::DPImm<Armv, op, s>;
+			
+			constexpr DataProcessing(cpu::reg rd,              cpu::reg rm) :
+				DataProcessing(rd, DPDefaultRN(op, rd), rm)
+			{}
+	
+			constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm) :
+				DataProcessing(rd, rn, rm, parts::shift::lsl, 0)
+			{}
+	
+			constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) :
+				DPImmShift(rd, rn, rm, sh, shift_imm), DPRegShift(), DPImm()
+			{}
+	
+			//--------------------------------------------------------------------------------------------
+	
+			constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) :
+				DPImmShift(), DPRegShift(rd, rn, rm, sh, rs), DPImm()
+			{}
+	
+			//--------------------------------------------------------------------------------------------
+	
+			constexpr DataProcessing(cpu::reg rd,              u8 rot, u8 imm) :
+				DataProcessing(rd, rd, rot, imm)
+			{}
+	
+			constexpr DataProcessing(cpu::reg rd, cpu::reg rn, u8 rot, u8 imm) :
+				DPImmShift(), DPRegShift(), DPImm(rd, rn, rot, imm)
+			{}
+	
+			constexpr operator u32() const {
+			//shadow the operator u32 in the super classes
+				return DPImmShift::getConditionalVal()
+						| DPRegShift::getConditionalVal()
+						| DPImm::getConditionalVal();
+			}
+	
+			constexpr u32 getVer() const {
+			//As mentioned above, this will or all of the conditional versions together,
+				return DPImmShift::getConditionalVer()
+						| DPRegShift::getConditionalVer()
+						| DPImm::getConditionalVer();
+			}
+		};
 
-		constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm) :
-			DataProcessing(rd, rn, rm, parts::shift::lsl, 0)
-		{}
-
-		constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, u8 shift_imm) :
-			DPImmShift(rd, rn, rm, sh, shift_imm), DPRegShift(), DPImm()
-		{}
-
-		//--------------------------------------------------------------------------------------------
-
-		constexpr DataProcessing(cpu::reg rd, cpu::reg rn, cpu::reg rm, parts::shift sh, cpu::reg rs) :
-			DPImmShift(), DPRegShift(rd, rn, rm, sh, rs), DPImm()
-		{}
-
-		//--------------------------------------------------------------------------------------------
-
-		constexpr DataProcessing(cpu::reg rd,              u8 rot, u8 imm) :
-			DataProcessing(rd, rd, rot, imm)
-		{}
-
-		constexpr DataProcessing(cpu::reg rd, cpu::reg rn, u8 rot, u8 imm) :
-			DPImmShift(), DPRegShift(), DPImm(rd, rn, rot, imm)
-		{}
-
-		constexpr operator u32() const {
-		//shadow the operator u32 in the super classes
-			return DPImmShift::getConditionalVal()
-					| DPRegShift::getConditionalVal()
-					| DPImm::getConditionalVal();
+		constexpr u32 BImmEnc(s32 off) {
+			u32 val = static_cast<u32>(off >> 2);
+			return val & mask::lower<24>::m;
 		}
 
-		constexpr u32 getVer() const {
-		//As mentioned above, this will or all of the conditional versions together,
-			return DPImmShift::getConditionalVer()
-					| DPRegShift::getConditionalVer()
-					| DPImm::getConditionalVer();
-		}
-	};
+		template<u32 Armv, parts::link l>
+		struct BranchImm : ArmInst<Armv, mask::BranchImm> {
+			constexpr BranchImm(s32 off) : ArmInst<Armv, mask::DPImmShift>(BImmEnc(off)) 
+			{}
+		};
 
-	#define glue(x,y) x ## y
+	} //namespace arm::ins::types
 
-	#define DefDPInstSet(ins)																							\
-		using glue(ins,  ) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::al>;	\
-		using glue(ins,eq) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::eq>;	\
-		using glue(ins,ne) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::ne>;	\
-		using glue(ins,cs) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::cs>;	\
-		using glue(ins,cc) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::cc>;	\
-		using glue(ins,mi) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::mi>;	\
-		using glue(ins,pl) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::pl>;	\
-		using glue(ins,vs) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::vs>;	\
-		using glue(ins,vc) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::vc>;	\
-		using glue(ins,hi) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::hi>;	\
-		using glue(ins,ls) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::ls>;	\
-		using glue(ins,ge) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::ge>;	\
-		using glue(ins,lt) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::lt>;	\
-		using glue(ins,gt) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::gt>;	\
-		using glue(ins,le) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::ignore>, parts::cond::le>;	\
-																														\
-		using glue(ins,S) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::al>;		\
-		using glue(ins,eqS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::eq>;	\
-		using glue(ins,neS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::ne>;	\
-		using glue(ins,csS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::cs>;	\
-		using glue(ins,ccS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::cc>;	\
-		using glue(ins,miS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::mi>;	\
-		using glue(ins,plS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::pl>;	\
-		using glue(ins,vsS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::vs>;	\
-		using glue(ins,vcS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::vc>;	\
-		using glue(ins,hiS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::hi>;	\
-		using glue(ins,lsS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::ls>;	\
-		using glue(ins,geS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::ge>;	\
-		using glue(ins,ltS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::lt>;	\
-		using glue(ins,gtS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::gt>;	\
-		using glue(ins,leS) = Conditional<DataProcessing<1, parts::dp::ins, parts::status::update>, parts::cond::le>
+	#define glue(x,y) x##y
+	#define DEF_INST(ins, ...)											\
+	using ins = types::Conditional< __VA_ARGS__ , parts::cond::al>;		\
+	template<parts::cond cond>											\
+	using glue(ins,_cond) = types::Conditional< __VA_ARGS__ , cond>
 
-	DefDPInstSet(And);
-	DefDPInstSet(Eor);
-	DefDPInstSet(Sub);
-	DefDPInstSet(Rsb);
-	DefDPInstSet(Add);
-	DefDPInstSet(Adc);
-	DefDPInstSet(Sbc);
-	DefDPInstSet(Rsc);
-	DefDPInstSet(Tst);
-	DefDPInstSet(Teq);
-	DefDPInstSet(Cmp);
-	DefDPInstSet(Cmn);
-	DefDPInstSet(Orr);
-	DefDPInstSet(Mov);
-	DefDPInstSet(Bic);
-	DefDPInstSet(Mvn);
+	#define DP__INST(i)																		\
+		DEF_INST(i, types::DataProcessing<1, parts::dp::i, parts::status::ignore>);				\
+		DEF_INST(i ## S, types::DataProcessing<1, parts::dp::i, parts::status::update>)
+
+	DP__INST	(Adc);
+	DP__INST	(Add);
+	DP__INST	(And);
+	DEF_INST	(B,  types::BranchImm<1, parts::link::ignore>);
+	DP__INST	(Bic);
+	DEF_INST	(Bl, types::BranchImm<1, parts::link::link	>);
+	DP__INST	(Cmn);
+	DP__INST	(Cmp);
+	DP__INST	(Eor);
+	DP__INST	(Mov);
+	DP__INST	(Mvn);
+	DP__INST	(Orr);
+	DP__INST	(Rsb);
+	DP__INST	(Rsc);
+	DP__INST	(Sbc);
+	DP__INST	(Sub);
+	DP__INST	(Teq);
+	DP__INST	(Tst);
+
 
 	//16 down, ~130 to go
 
+	#undef glue
+	#undef DEFDPINST
+	#undef DEFINST
 } //namespace arm::ins
 
 #endif //ARM_INS_H
